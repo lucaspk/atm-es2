f0a372c0db27118dd1b8d414a2ebc77d779b641b
aryssonc <arysson.cavalcanti@gmail.com>
2017-07-07 11:14:10 -0300

    Testes desaldo adicionados

---
+++ src/testes/BalanceTest.java
@@ -0,0 +1,37 @@
+package testes;
+
+import org.junit.*;
+
+import api.ATMExec;
+import api.AbstractOperation;
+import api.AbstractOperation.BalanceInquiry;
+
+public class BalanceTest {
+
+	private ATMExec atm;
+	private BalanceInquiry operation;
+	
+	@Before
+	public void setUp() {
+		atm = new ATMExec();
+		operation = balancePattern();
+	}
+	
+	private BalanceInquiry balancePattern() {
+		atm.defineOperation("balance");
+		BalanceInquiry operation = (BalanceInquiry)atm.getOperation();
+		return operation;
+	}
+	
+	@Test
+	public void invalidCardTest() {
+		Assert.assertEquals("Invalid card", operation.operateInCheckings(123, 123)); 
+	}
+	
+	@Test
+	public void validCardTest() {
+		double cash = AbstractOperation.getAmountOfCashFromAccount("checking");
+		Assert.assertEquals("TOTAL BAL[checking]: $" + cash, operation.operateInCheckings(1, 42));
+	}
+
+}
